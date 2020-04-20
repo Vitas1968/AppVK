6c98b6288ada1e2ad82e17c4aa101e6e5a0b0757
Vitas1968 <dor1968@mail.ru>
2020-04-20 23:10:04 +0700

    меняем интерфейс

--- app/src/main/java/com/google/vitaly/appvk/mvp/view/list/UserItemView.kt
+++ app/src/main/java/com/google/vitaly/appvk/mvp/view/list/UserItemView.kt
@@ -2,6 +2,9 @@ package com.google.vitaly.appvk.mvp.view.list
 
 interface UserItemView {
     var pos: Int
-    fun setLogin(text: String)
+    fun setFirstName(firstName: String)
+    fun setLastName(lastName: String)
+    fun setCity(cityName: String)
+
     fun loadAvatar(url: String)
 }
